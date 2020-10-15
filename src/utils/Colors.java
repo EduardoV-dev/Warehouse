45dea2d6ba2f9785e97582a17843031bde9629ad
FabriDevelop <cfar.dev@gmail.com>
2020-10-15 14:02:09 -0600

    Se agregó paleta de colores

---
+++ src/utils/Colors.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.awt.*;
+
+public class Colors {
+    public static final Color AZUL_OSCURO = new Color(54, 79, 107);
+    public static final Color CELESTE = new Color(63, 209, 218);
+    public static final Color ROJO = new Color(252, 81, 133);
+    public static final Color GRIS_CLARO = new Color(224, 224, 224);
+}
